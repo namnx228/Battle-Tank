914bca802137b1c2a8224b7a29739a87b3d058f2
minhle <minhle2994@gmail.com>
2013-08-11 22:10:54 +0700

    AI hunter

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -3,7 +3,7 @@
 using namespace cocos2d;
 
 int bulletcount = 0;
-
+float tankSize;
 CCScene* HelloWorld::scene()
 {
     // 'scene' is an autorelease object
@@ -31,8 +31,6 @@ bool HelloWorld::init()
 	moveSize = size.width/4;
 	m_IsTouchMoved = false;
 	IsHolding = false;
-	win = false;
-	lose = false;
 	setTouchEnabled(true);
 	scheduleUpdate();
 
@@ -46,24 +44,33 @@ bool HelloWorld::init()
 	this->addChild(_background, -5);
 	this->addChild(_tree, 5);
 //	_background->removeTileAt( ccp(3.0, 3.0) );
+	tileSize = _tileMap->getTileSize().width;
 
 	//Add tank
 	Player1.m_appearance = CCSprite::create("tank1.png");
 	this->addChild(Player1.m_appearance,1);
 	Player1.m_appearance->setPosition(ccp(size.width/4,Player1.m_appearance->getContentSize().height/2));
 	Player1.initTank();
+	tankSize = Player1.m_appearance->getContentSize().width;
+
+	Player2.m_appearance = CCSprite::create("tank1.png");
+	this->addChild(Player2.m_appearance,1);
+	Player2.m_appearance->setPosition(ccp(size.width/4,size.height - Player2.m_appearance->getContentSize().height/2));
+	Player2.m_appearance->setRotation(180);
+	Player2.initTank();
+	Player2.m_timedelay = 0;
 
 	//Add Citadel
 	ourCitadel.m_appearance = CCSprite::create("thanh.png");
 	this->addChild(ourCitadel.m_appearance,1);
 	ourCitadel.m_appearance->setPosition(ccp(size.width/2,ourCitadel.m_appearance->getContentSize().height/2));
 	ourCitadel.m_hp = 50; 
+	ourCitadel.m_ok = true;
 
 	enemyCitadel.m_appearance = CCSprite::create("thanh.png");
 	this->addChild(enemyCitadel.m_appearance,1);
 	enemyCitadel.m_appearance->setPosition(ccp(size.width/2,size.height - enemyCitadel.m_appearance->getContentSize().height/2));
 	enemyCitadel.m_hp = 50; 
-	
 
 	//Add virtual control
 	smallcircle = CCSprite::create("nho.png");
@@ -74,6 +81,12 @@ bool HelloWorld::init()
 	this->addChild(bigcircle,10);
 	bigcircle->setVisible(false);	
 
+	//Add item
+	speedUp.m_appearance = CCSprite::create("speedUpItem.png");
+	this->addChild(speedUp.m_appearance, 5);
+	speedUp.m_appearance->setPosition(ccp(size.width/4, size.height*3/4));
+	speedUp.m_ok = true;
+	speedUp.initItem(0, 1, 0, 1200, 0);
 	return true;
 }
 
@@ -92,7 +105,6 @@ void HelloWorld::ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent)
 	}
 	else
 		IsHolding = true;
-
 }
 
 void HelloWorld::ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent)
@@ -148,63 +160,64 @@ void HelloWorld::ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent)
 
 void HelloWorld::update(float pDt)
 {
-	if (m_IsTouchMoved)
+	if (Player1.m_ok)
 	{
-		CCPoint bigPos = bigcircle->getPosition();
-		CCPoint smallPos = smallcircle->getPosition();
-		m_DirectionVector = ccpSub(smallPos, bigPos);	
-		m_DirectionVector = normalize(m_DirectionVector, bigPos, smallPos);
-		Player1.m_direction = m_DirectionVector;
-		move(Player1);
+		if ( m_IsTouchMoved)
+		{
+			CCPoint bigPos = bigcircle->getPosition();
+			CCPoint smallPos = smallcircle->getPosition();
+			m_DirectionVector = ccpSub(smallPos, bigPos);	
+			m_DirectionVector = normalize(m_DirectionVector, bigPos, smallPos);
+			Player1.m_direction = m_DirectionVector;
+			move(Player1);
+		}
+		fire(Player1, IsHolding);
 	}
-	
-	fire(Player1, IsHolding);
+
+	if (Player2.m_ok)
+	{
+		if (Player2.m_timedelay == 0 && Player1.m_ok)
+		{
+			bfs(Player2, Player1.m_appearance->getPosition());
+			Player2.m_timedelay = 300;
+		}
+		if (Player2.m_timedelay > 0) Player2.m_timedelay -= 1;
+
+		bool ok_fire = false;
+		CCPoint oldDirection = Player2.m_direction;
+		ok_fire = ok_fire || (ourCitadel.m_ok && checkFire(Player2, ourCitadel.m_appearance));
+		ok_fire = ok_fire || (Player1.m_ok && checkFire(Player2, Player1.m_appearance));
+		fire(Player2, ok_fire);
+		Player2.m_direction = oldDirection;
+		if (!ok_fire && Player2.count > 1)
+		{
+			setDirection(Player2);
+			move(Player2);
+		}
+	}
 	checkBullet();
 }
 
 void HelloWorld::checkBullet()
 {
-	for(int i=0;i<m_stack.size();i++)
+	for(int i=0; i<m_stack.size(); i++)
 	{	
 		CCPoint pos = m_stack[i].m_appearance->getPosition();
 		CCPoint tileCoord = tileCoordForPosition(pos);
 		if (!check(pos)) 
 			{
 				CCString *type = getType(pos);
-				if (type->compare("Water") == 0 || type->compare("Brick") == 0 || type->compare("Wall") == 0)
+				if (type != NULL && (type->compare("Brick") == 0 || type->compare("Wall") == 0))
 				{
 					if (type->compare("Brick") == 0) 
-						{
 							_background->removeTileAt( tileCoord );
-							m_stack[i].m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
-						}
-
-					if (type->compare("Wall") == 0) 
-							m_stack[i].m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
+					m_stack[i].m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
 				}
-			}	
-
-		if (!win && !lose && m_stack[i].m_appearance->boundingBox().intersectsRect(enemyCitadel.m_appearance->boundingBox()))
-			{
-				enemyCitadel.m_hp -= m_stack[i].m_dame;
-				if (enemyCitadel.m_hp <= 0)
-				{
-					enemyCitadel.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
-					win = true;
-				}
-				m_stack[i].m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));				
 			}
-
-		if (!win && !lose && m_stack[i].m_appearance->boundingBox().intersectsRect(ourCitadel.m_appearance->boundingBox()))
-			{
-				ourCitadel.m_hp -= m_stack[i].m_dame;
-				if (ourCitadel.m_hp <= 0)
-				{
-					ourCitadel.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
-					lose = true;
-				}
-				m_stack[i].m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));				
-			}
+		setEffect(m_stack[i], enemyCitadel);
+		setEffect(m_stack[i], ourCitadel);
+		setEffect(m_stack[i], Player1);
+		setEffect(m_stack[i], Player2);
 	}
 }
 
@@ -212,36 +225,43 @@ void HelloWorld::fire(Tank &player, bool ok)
 {
 	if ((ok) && (player.m_time == 0)) 
 	{
+		player.m_appearance->setRotation(toAngle(player.m_direction));
 		Bullet dame;
 		dame.m_appearance = CCSprite::create("dame.png");
 		dame.m_dame = player.m_dame;
 		dame.m_speed = player.m_speed;
 		this->addChild(dame.m_appearance,4);
 		CCPoint pos = player.m_appearance->getPosition();
+		pos.x = pos.x + player.m_direction.x*(tankSize/2+5);
+		pos.y = pos.y + player.m_direction.y*(tankSize/2+5);
+
 		dame.m_appearance->setPosition(pos);
 		dame.m_appearance->setTag(bulletcount++);
 		m_stack.push_back(dame);
-
-		if ( toAngle(player.m_direction) == 0) dame.m_appearance->runAction(CCSequence::create(CCMoveBy::create(2, ccp(0, 300)), CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)), NULL));
-		if ( toAngle(player.m_direction) == 90) dame.m_appearance->runAction(CCSequence::create(CCMoveBy::create(2, ccp(300, 0)), CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)), NULL));
-		if ( toAngle(player.m_direction) == 180) dame.m_appearance->runAction(CCSequence::create(CCMoveBy::create(2, ccp(0, -300)), CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)), NULL));
-		if ( toAngle(player.m_direction) == 270) dame.m_appearance->runAction(CCSequence::create(CCMoveBy::create(2, ccp(-300, 0)), CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)), NULL));		
+		dame.m_appearance->runAction(CCSequence::create(CCMoveBy::create(2, ccp(300*player.m_direction.x, 300*player.m_direction.y)), CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)), NULL));
 		player.m_time = 50;	
 	}
+	if (player.m_item.m_time == 0) player.m_item.initItem(0, 0, 0, -1, 0);
 	if (player.m_time) player.m_time -= 1;	
-	
+	if (player.m_item.m_time) player.m_item.m_time -= 1;
 }
 
-void HelloWorld::move(Tank player)
+void HelloWorld::move(Tank &player)
 {
 	player.m_appearance->setRotation(toAngle(player.m_direction));
 	
 	CCPoint pos = player.m_appearance->getPosition();
 	
 	CCPoint nextPos;
-	nextPos.x = pos.x + player.m_direction.x*player.m_speed;
-	nextPos.y = pos.y + player.m_direction.y*player.m_speed;
-	float tankSize = player.m_appearance->getContentSize().width;
+	nextPos.x = pos.x + player.m_direction.x*(player.m_speed + player.m_item.m_speed);
+	nextPos.y = pos.y + player.m_direction.y*(player.m_speed + player.m_item.m_speed);
+
+	if (speedUp.m_ok && player.m_appearance->boundingBox().intersectsRect(speedUp.m_appearance->boundingBox()))
+	{
+		player.m_item = speedUp;
+		speedUp.m_appearance->setVisible(false);
+		speedUp.m_ok = false;
+	}
 
 	player.m_checkPoint1 = ccpAdd(nextPos, ccp(-tankSize/2, tankSize/2));
 	player.m_checkPoint2 = ccpAdd(nextPos, ccp(tankSize/2, tankSize/2));
@@ -261,6 +281,71 @@ void Tank::initTank()
 	m_speed = 1;
 	m_hp = 40;
 	m_direction = ccp(0, 1);
+	m_item.m_speed = 0;
+}
+
+void HelloWorld::setDirection(AI &ai)
+{
+	int tmp = 0;
+	ai.m_checkPoint1 = tileCoordForPosition(ccpAdd(ai.m_appearance->getPosition(), ccp(-tankSize/2, tankSize/2)));
+	ai.m_checkPoint2 = tileCoordForPosition(ccpAdd(ai.m_appearance->getPosition(), ccp(tankSize/2, tankSize/2)));
+	ai.m_checkPoint3 = tileCoordForPosition(ccpAdd(ai.m_appearance->getPosition(), ccp(tankSize/2, -tankSize/2)));
+	ai.m_checkPoint4 = tileCoordForPosition(ccpAdd(ai.m_appearance->getPosition(), ccp(-tankSize/2, -tankSize/2)));
+
+	if (ai.m_checkPoint1.x != ai.go[ai.count].x || ai.m_checkPoint1.y != ai.go[ai.count].y) tmp++;
+	if (ai.m_checkPoint2.x != ai.go[ai.count].x || ai.m_checkPoint2.y != ai.go[ai.count].y) tmp++;
+	if (ai.m_checkPoint3.x != ai.go[ai.count].x || ai.m_checkPoint3.y != ai.go[ai.count].y) tmp++;
+	if (ai.m_checkPoint4.x != ai.go[ai.count].x || ai.m_checkPoint4.y != ai.go[ai.count].y) tmp++;
+
+	if (tmp == 4) ai.count--;
+	if (ai.count > 1)
+		ai.m_direction = ccp(ai.go[ai.count - 1].x - ai.go[ai.count].x, ai.go[ai.count - 1].y - ai.go[ai.count].y);
+
+	if (ai.m_direction.x == 0)
+		ai.m_direction.y *= -1;
+}
+
+bool HelloWorld::checkFire(AI &ai, CCSprite *target)
+{
+	float subX = ai.m_appearance->getPosition().x - target->getPosition().x;
+	float subY = ai.m_appearance->getPosition().y - target->getPosition().y;
+
+	if (abs(subX) < 10 && abs(subY) < 300)
+	{
+		if (subY > 0)
+			ai.m_direction = ccp(0, -1);
+		else
+			ai.m_direction = ccp(0, 1);
+		if (!canFire(ai.m_appearance->getPosition(), ai.m_direction, (int)abs(subY)/tileSize)) return false;
+		return true;
+	}
+
+	if (abs(subY) < 10 && abs(subX) < 300)
+	{
+		if (subX > 0)
+			ai.m_direction = ccp(-1, 0);
+		else
+			ai.m_direction = ccp(1, 0);
+		if (!canFire(ai.m_appearance->getPosition(), ai.m_direction, (int)abs(subX)/tileSize)) return false;
+		return true;
+	}
+	return false;
+}
+
+bool HelloWorld::canFire(CCPoint pos, CCPoint direction, int tiles)
+{
+	int tmp = 0;
+	for (int i=0; i<tiles; i++)
+	{
+		pos.x += direction.x * tileSize;
+		pos.y += direction.y * tileSize;
+		CCString *string = getType(pos);
+		if (string != NULL && string->compare("Wall") == 0) return false;
+		if (string != NULL && string->compare("Brick") == 0) tmp++;
+	}
+	if (tmp < 3)
+		return true;
+	return false;
 }
 
 float HelloWorld::toAngle(CCPoint direction)
@@ -290,6 +375,8 @@ CCPoint HelloWorld::tileCoordForPosition(CCPoint pos)
 
 CCString *HelloWorld::getType(CCPoint pos)
 {
+	if (pos.x < 0 || pos.x >= size.width || pos.y < 0 || pos.y >= size.height)
+		return NULL;
 	CCPoint tileCoord = tileCoordForPosition(pos);
 	int tileGid = _background->tileGIDAt(tileCoord);
 	if (tileGid == 0) return NULL;
@@ -329,6 +416,33 @@ void HelloWorld::removeObject( CCNode* sender )
 	sender->removeFromParentAndCleanup(true);
 }
 
+void HelloWorld::setEffect(Bullet &bullet, Tank &tank)
+{
+	if (tank.m_ok && bullet.m_appearance->boundingBox().intersectsRect(tank.m_appearance->boundingBox()))
+	{
+		tank.m_hp -= bullet.m_dame;
+		if (tank.m_hp <= 0)
+		{
+			tank.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
+			tank.m_ok = false;
+		}
+		bullet.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
+	}
+}
+
+void HelloWorld::setEffect(Bullet &bullet, Citadel &citadel)
+{
+	if (citadel.m_ok && bullet.m_appearance->boundingBox().intersectsRect(citadel.m_appearance->boundingBox()))
+	{
+		citadel.m_hp -= bullet.m_dame;
+		if (citadel.m_hp <= 0)
+		{
+			citadel.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
+			citadel.m_ok = false;
+		}
+		bullet.m_appearance->runAction( CCCallFuncN::create(this, callfuncN_selector(HelloWorld::removeObject)));
+	}
+}
 
 CCPoint HelloWorld::normalize(CCPoint m_DirectionVector, CCPoint bigPos, CCPoint smallPos)
 {
@@ -361,6 +475,51 @@ CCPoint HelloWorld::normalize(CCPoint m_DirectionVector, CCPoint bigPos, CCPoint
 			return(ccp(0, -1));
 }
 
+void HelloWorld::bfs(AI &ai, CCPoint target)
+{
+	CCPoint dir[5];
+	dir[1] = ccp(1, 0); dir[2] = ccp(0, 1); dir[3] = ccp(0, -1); dir[4] = ccp(-1, 0);
+
+	CCPoint pos = ai.m_appearance->getPosition();
+	if (ai.m_direction.x > 0|| ai.m_direction.y > 0) 
+		pos = ccpAdd(pos, ccp(-tankSize/2, -tankSize/2));
+	else 
+		pos = ccpAdd(pos, ccp(tankSize/2, tankSize/2));
+
+	memset(free, 0, sizeof(free));
+	CCPoint s = tileCoordForPosition(pos);
+	CCPoint f = tileCoordForPosition(target);
+
+	free[(int)s.x][(int)s.y] = 1;
+	q[1] = s;
+	int last = 1;
+	int first = 1;
+	ai.count = 0;
+
+	while (first <= last)
+	{
+		CCPoint u = q[first++];
+		for (int i=1; i<5; i++)
+		{
+			CCPoint v;
+			v.x = u.x + dir[i].x;
+			v.y = u.y + dir[i].y;
+			if (check(ccp(v.x*_tileMap->getTileSize().width, size.height-1 - v.y*32)) &&v.x >= 0 && v.x < 32 && v.y >=0 && v.y < 20 && free[(int)v.x][(int)v.y] == 0)
+			{
+				q[++last] = v;
+				free[(int)v.x][(int)v.y] = 1;
+				trace[(int)v.x][(int)v.y] = u;
+			}
+		}
+	}
+	
+	while (f.x != s.x || f.y != s.y)
+	{
+		ai.go[++ai.count] = f;
+		f = trace[(int)f.x][(int)f.y];
+	}
+	ai.go [++ai.count] = s;
+}
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -12,7 +12,18 @@ class Bullet
 public:
 	CCSprite *m_appearance;
 	float m_dame, m_speed;
-	int id;
+};
+
+class Item
+{
+public:
+	CCSprite *m_appearance;
+	float m_dame, m_speed, m_hp, m_time;
+	float m_delaySpeed;
+	bool m_ok;
+
+	void initItem(float dame, float speed, float hp, float time, float delaySpeed)
+	{m_dame = dame; m_speed = speed; m_hp = hp; m_time = time; m_delaySpeed = delaySpeed;}
 };
 
 class Tank
@@ -20,33 +31,48 @@ class Tank
 public:
 	CCSprite *m_appearance;
 	float m_time, m_dame, m_speed, m_hp;
+	bool m_ok;
 	CCPoint m_direction, m_checkPoint1, m_checkPoint2, m_checkPoint3, m_checkPoint4;
+	Item m_item;
 
 	void initTank();
 };
 
-class citadel
+class Citadel
 {
 public:
 	CCSprite *m_appearance;
 	long m_hp;
+
+	bool m_ok;
+};
+
+class AI : public Tank
+{
+public:
+	int count, m_timedelay;
+	CCPoint go[400];
 };
 
 class HelloWorld : public cocos2d::CCLayer
 {
 private:
-	Tank Player1;
-	CCSize size;
-	CCSprite* bigcircle;
-	CCSprite* smallcircle;
-	CCPoint m_DirectionVector;
-	float moveSize;
-	bool IsHolding, m_IsTouchMoved;
 	CCTMXTiledMap *_tileMap;
 	CCTMXLayer *_background, *_tree;
-	bool win, lose;
-	citadel ourCitadel, enemyCitadel;
+	Tank Player1;
+	AI Player2;
+	Citadel ourCitadel, enemyCitadel;
+	Item speedUp;
+
+	CCSize size;
+	CCSprite *bigcircle, *smallcircle;
+	CCPoint m_DirectionVector;
+	float moveSize, tankSize, tileSize;
+	bool IsHolding, m_IsTouchMoved;
 	vector<Bullet> m_stack;
+
+	int free[32][22];
+	CCPoint q[1000], trace[32][22];
 
 public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
@@ -63,27 +89,34 @@ public:
 	void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
 	void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
 	void ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent);
-	void move(Tank player);
+
+	//Function for player
+	void move(Tank &player);
 	void fire(Tank &player, bool ok);
 	void checkBullet();
 	void removeObject( CCNode* sender);
+
 	CCPoint normalize(CCPoint m_DirectionVector, CCPoint bigPos, CCPoint smallPos);
 	bool ok_go(CCString *type);
 	bool check(CCPoint pos);
 	float toAngle(CCPoint direction);
 	CCString *getType(CCPoint pos);
 	CCPoint tileCoordForPosition(CCPoint position);
+	
+
+	void setEffect(Bullet &bullet, Tank &tank);
+	void setEffect(Bullet &bullet, Citadel &citadel);
+
+	//Function for AI
+	void bfs(AI &ai, CCPoint target); 
+	void setDirection(AI &ai);
+	bool canFire(CCPoint pos1, CCPoint pos2, int tiles);
+	bool checkFire(AI &ai, CCSprite *target);
 
 	void update(float pDt);
 
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
 };
-
-bool ok_go(CCString *type);
-bool check(CCPoint pos);
-float toAngle(CCPoint direction);
-CCString *getType(CCPoint pos);
-CCPoint tileCoordForPosition(CCPoint position);
 
 #endif // __HELLOWORLD_SCENE_H__
